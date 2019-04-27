f8601a0d0dda77ffd6a2725864afc6c4ae4c584f
Daniel Maldonado <daniel.maldonado@bluealba.com>
2019-04-27 20:55:10 -0300

    join related tests into one file

---
+++ src/RenderedValue.spec.js
@@ -0,0 +1,26 @@
+import setUp from './setUp';
+import { cleanup } from 'react-testing-library';
+
+const MOCK_VALUE = "hello";
+
+beforeEach(cleanup)
+
+test('should render the value passed in by calling it', () => {
+
+	const value = jest.fn(() => MOCK_VALUE)
+	const { renderedValueContent } = setUp({value})
+	
+	expect(value).toHaveBeenCalled()
+	expect(renderedValueContent()).toEqual(value())
+});
+
+test('should hide rendered value when clicks on it and show editing value', () => {
+
+	const value = jest.fn(() => MOCK_VALUE)
+	const { clickOnRenderedValue, editingValueContent, renderedValue } = setUp({value})
+
+	clickOnRenderedValue()
+
+	expect(renderedValue()).toBeNull()
+	expect(editingValueContent()).toEqual(value())
+});
